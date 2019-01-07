547ee560a661667569393a05ca0e5d820510e468
Feyzullah YILDIZ <feyzullahyildiz@Feyzullahs-Mac-Pro.local>
2019-01-07 22:19:29 +0300

    Orta noktalar şehirlerin daha müsait yerlerine çekildi. menünün yeri sabitleştirildi. iller tıklanılarak seçilebilinir oldu. toplam cami sayısı eklendi.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const cities = require('./iller.json')
 const centroids = require('./iller-orta.json')
 const config = require('./config')
-const container = document.querySelector('#turkey-svg-citiescontiner .container')
-const menu = document.querySelector('#turkey-svg-citiescontiner .menu')
+const map = document.querySelector('#turkey-svg-cities-continer .map-container .map')
+const mapHeader = document.querySelector('#turkey-svg-cities-continer .map-container .map-header')
+const menu = document.querySelector('#turkey-svg-cities-continer .map-menu')
 
 // [ 2857405, 4275017 ], [ 2857405, 5175729 ], [ 4989109, 5175729 ], [ 4989109, 4275017 ], [ 2857405, 4275017 ] //bbox
 const loadSvg = function(data){
     const xSpace = 1420
     const ySpace = 2590
     let pointArray = ''
+    let totalMosqueCount = 0
     for(const index in cities.features){
         const feature = cities.features[index]
         const code = centroids.features[index].properties.ILKOD
@@ -24,30 +26,45 @@ const loadSvg = function(data){
             }
             pointArray +=`<polygon points="${points}"/>`
         }
+        if(data[code] && data[code].sayi){
+            totalMosqueCount += data[code].sayi
+        }
         pointArray += `
         <text x="${x}" y="${y}" fill="white">
             <tspan text-anchor="middle">${data[code] ? (data[code].a + data[code].b + data[code].c): '-'}</tspan>
             </text>
-            <desc>${data[code] ? JSON.stringify({...data[code], name}): ''}</desc>
+            <desc>${data[code] ? JSON.stringify({...data[code], name, success: true}): JSON.stringify({name})}</desc>
         </g>`
+    }
+    if(!isNaN(totalMosqueCount)){
+        mapHeader.innerHTML = 'Toplam Cami ' + totalMosqueCount
     }
     let svg = `
     <svg viewBox="0 0 1080 460" width="100%">
     ${pointArray}
     </svg>
     `
-    container.innerHTML = svg
+    map.innerHTML = svg
     
     const [city, categoryA, categoryB, categoryC, categoryTotal, countOfMosque] = menu.querySelectorAll('li span')
     // console.log('city', city)
-    const citiesPolygons = container.querySelectorAll('svg g')
+    const citiesPolygons = map.querySelectorAll('svg g')
+    let oldPolygons
     const onEnter = function(){
+        if(oldPolygons){
+            oldPolygons.forEach(oldPolygon => {
+                oldPolygon.style.fill = ''
+            })
+        }
+        oldPolygons = []
         this.querySelectorAll('polygon').forEach(polygon => {
-            polygon.style.fill = '#ff922b'
+            oldPolygons.push(polygon)
+            polygon.style.fill = '#f49225'
         })
         const data = this.querySelector('desc')
-        if(data.innerHTML){
-            menu.style.display = 'flex'
+        if(data.innerHTML && JSON.parse(data.innerHTML).success){
+            // menu.style.display = 'flex'
+            menu.style.opacity = 1
             const cityData = JSON.parse(data.innerHTML)
             city.innerHTML = cityData.name
             categoryA.innerHTML = cityData.a
@@ -55,22 +72,30 @@ const loadSvg = function(data){
             categoryC.innerHTML = cityData.c
             categoryTotal.innerHTML = cityData.a + cityData.b + cityData.c
             countOfMosque.innerHTML = cityData.sayi
+        }else{
+            // menu.style.opacity = 0
+            city.innerHTML = JSON.parse(data.innerHTML).name
+            categoryA.innerHTML = 0
+            categoryB.innerHTML = 0
+            categoryC.innerHTML = 0
+            categoryTotal.innerHTML = 0
+            countOfMosque.innerHTML = 0
         }
     }
     const onLeave = function(){
         this.querySelectorAll('polygon').forEach(polygon => {
             polygon.style.fill = ''
         })
-        menu.style.display = 'none'
+        menu.style.opacity = 0
     }
-    const onMove = function(event){
-        menu.style.left = event.clientX + 30
-        menu.style.top = event.clientY
-    }
+    // const onMove = function(event){
+    //     menu.style.left = event.clientX + 30
+    //     menu.style.top = event.clientY
+    // }
     citiesPolygons.forEach(city => {
-        city.addEventListener('mouseenter', onEnter)
-        city.addEventListener('mouseleave', onLeave)
-        city.addEventListener('mousemove', onMove)
+        city.addEventListener('click', onEnter)
+        // city.addEventListener('mouseleave', onLeave)
+        // city.addEventListener('mousemove', onMove)
     })
 }
 
